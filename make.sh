64b17382e4a8e27e0c8a0702ce8bfee0d2e7faad
jmeyering <jared.meyering@gmail.com>
2014-06-10 19:46:26 -0500

    updates to the make script

---
+++ make.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files="bashrc vimrc tmux.conf vim"    # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+mkdir -p $olddir
+
+# change to the dotfiles directory
+cd $dir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+for file in $files; do
+    mv ~/.$file ~/$olddir 2>/dev/null
+    ln -s $dir/$file ~/.$file 2>/dev/null
+done
+echo "done"
