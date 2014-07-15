0427179750f33d2275b69821d72d71a41cb019d7
jaredatblend <jared@blendimc.com>
2014-07-15 02:03:53 +0000

    fix dir issue

--- make.sh
+++ make.sh
@@ -7,24 +7,25 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-olddir=~/old_dotfiles             # old dotfiles backup directory
-dotfiles="bashrc bash_aliases vimrc tmux.conf vim todo_completion"    # list of files/folders to symlink in homedir
-regfiles="todo_completion todo.sh todo.cfg"    # list of files/folders to symlink in homedir
+
+dotfiles="bashrc bash_aliases vimrc tmux.conf todo_completion"    # list of filesto symlink in homedir
+regfiles="todo_completion todo.sh todo.cfg"    # list of filesto symlink in homedir
+dotdirs="vim"    # list of folders to symlink in homedir
 
 ##########
-
-# create dotfiles_old in homedir
-mkdir -p $olddir
 
 # change to the dotfiles directory
 cd $dir
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+#Create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $dotfiles; do
-    mv ~/.$file ~/$olddir 2>/dev/null
     ln -s $dir/$file ~/.$file 2>/dev/null
 done
 for file in $regfiles; do
     ln -s $dir/$file ~/$file 2>/dev/null
+done
+for folder in $dotdirs; do
+    rm -rf ~/.$folder 2>/dev/null
+    ln -s $dir/$folder ~/.$folder 2>/dev/null
 done
 echo "done"
